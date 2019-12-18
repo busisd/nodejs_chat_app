9c80d232c12ad36edf0a10ff25bfb996e583c985
busisd <busisd@carleton.edu>
2019-12-17 23:27:05 -0500

    The emotes GNOME and DOOT now work!

--- public/MessageContainer.js
+++ public/MessageContainer.js
@@ -6,14 +6,107 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+function MessageEmote(props) {
+	return React.createElement("img", { src: props.src, className: "inline-image" });
+}
+
+var emotes = [["GNOME", "gnome.png"], ["DOOT", "doot.jpg"]];
 function BasicMessage(props) {
+	var msg = props.message;
+	var msg_arr = [msg];
+	var img_key = 0;
+
+	var _iteratorNormalCompletion = true;
+	var _didIteratorError = false;
+	var _iteratorError = undefined;
+
+	try {
+		for (var _iterator = emotes[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+			emote_row = _step.value;
+
+			var emote = emote_row[0];
+			var index = 0;
+			while (index < msg_arr.length) {
+				if (typeof msg_arr[index] !== "string") {
+					index += 1;
+					continue;
+				}
+
+				var emote_loc = msg_arr[index].search(emote);
+				if (emote_loc === -1) {
+					index += 1;
+					continue;
+				}
+
+				before_emote = msg_arr[index].substring(0, emote_loc);
+				after_emote = msg_arr[index].substring(emote_loc + emote.length, msg_arr[index].length);
+				new_emote = React.createElement(MessageEmote, { src: emote_row[1], key: img_key });
+				msg_arr.splice(index, 1, before_emote, new_emote, after_emote);
+
+				img_key++;
+			}
+		}
+	} catch (err) {
+		_didIteratorError = true;
+		_iteratorError = err;
+	} finally {
+		try {
+			if (!_iteratorNormalCompletion && _iterator.return) {
+				_iterator.return();
+			}
+		} finally {
+			if (_didIteratorError) {
+				throw _iteratorError;
+			}
+		}
+	}
+
+	var final_elements_arr = [];
+	var _iteratorNormalCompletion2 = true;
+	var _didIteratorError2 = false;
+	var _iteratorError2 = undefined;
+
+	try {
+		for (var _iterator2 = msg_arr[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+			item = _step2.value;
+
+			if (typeof item === "string") {
+				if (item) {
+					final_elements_arr.push(item);
+				}
+			} else {
+				final_elements_arr.push(item);
+			}
+		}
+	} catch (err) {
+		_didIteratorError2 = true;
+		_iteratorError2 = err;
+	} finally {
+		try {
+			if (!_iteratorNormalCompletion2 && _iterator2.return) {
+				_iterator2.return();
+			}
+		} finally {
+			if (_didIteratorError2) {
+				throw _iteratorError2;
+			}
+		}
+	}
+
 	return React.createElement(
 		"li",
 		null,
 		" ",
-		props.name,
+		React.createElement(
+			"span",
+			{ className: "username" },
+			" ",
+			props.name
+		),
 		": ",
-		props.message,
+		msg_arr.map(function (msg_item) {
+			return msg_item;
+		}),
 		" "
 	);
 }
